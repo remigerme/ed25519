6d061950224c1ab1ac91384ce4b1e360e0d79c85
remi.germe <remi.germe@gmail.com>
2025-02-18 19:21:35 +0100

    initial sign implem

--- src/ed25519.c
+++ src/ed25519.c
@@ -29,6 +29,59 @@ void ed25519_keygen(uchar sk[32], uchar pk[32]) {
     mpz_clears(s, A, p, NULL);
 }
 
-void ed25519_sign(uchar sk[32], uchar pk[32], char *data, uchar sig[64]) {}
+void ed25519_sign(uchar sk[32], char *data, size_t data_size, uchar sig[64]) {
+    mpz_t s, A, p, L, r, k, Rm;
+    mpz_inits(s, A, p, L, r, k, Rm, NULL);
 
-int ed25519_verify(uchar pk[32], char *data, uchar sig[64]) {}
+    // Step 1
+    uchar pk[32];
+    uchar h[64];
+    sha3_512((char *)sk, 32, (char *)h);
+    decode_scalar_25519(h, s);
+    mpz_set_ui(p, 9);
+    curve25519_ladder(A, s, p);
+    encode_u_coord_25519(A, pk);
+
+    // Step 2
+    char *buf2 = (char *)malloc(32 + data_size);
+    memcpy(buf2, &h[32], 32);
+    memcpy(&buf2[32], data, data_size);
+    uchar rb[64];
+    sha3_512(buf2, 32 + data_size, (char *)rb);
+    decode_le(rb, 64 * 8, r);
+    free(buf2);
+
+    // Step 3
+    uchar R[32];
+    mpz_mod(r, r, L);
+    curve25519_ladder(Rm, r, p);
+    encode_u_coord_25519(Rm, R);
+
+    // Step 4
+    char *buf4 = (char *)malloc(64 + data_size);
+    memcpy(buf4, R, 32);
+    memcpy(&buf4[32], pk, 32);
+    memcpy(&buf4[64], data, data_size);
+    uchar kb[64];
+    sha3_512(buf4, 64 + data_size, (char *)kb);
+    decode_le(kb, 64 * 8, k);
+    free(buf4);
+
+    // Step 5
+    mpz_mod(k, k, L);
+    mpz_mul(k, k, s);
+    mpz_add(k, k, r);
+    mpz_mod(k, k, L); // k contains S at this point
+
+    // Step 6
+    memcpy(sig, R, 32);
+    char S[32];
+    mpz_to_chars(k, 32, S);
+    memcpy(&sig[32], S, 32);
+
+    mpz_clears(s, A, p, L, r, k, Rm, NULL);
+}
+
+/*
+int ed25519_verify(uchar pk[32], char *data, size_t data_size, uchar sig[64]) {}
+*/
